5c5828f365af90f34cca849c65aec737084c5d4a
Sneha Roy <sneha.dgp2014@gmail.com>
2021-08-11 22:48:35 +0530

    add searchbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Serach';
+
+
 
 function App() {
+  const [word, setWord] = useState('')
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+  }
+  
+  console.log(word);
   return (
     <div className="App">
       <Header title="Image Gallery"></Header>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}></Search>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import {Navbar} from 'react-bootstrap'
+import {Navbar, Container} from 'react-bootstrap'
+
+const navbarStyle = {
+    backgroundColor: 'lightblue',
+}
 
 const Header = (props) => {
     return (
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Navbar variant="light" style={navbarStyle}>
+            <Container>
+                <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+            </Container>
+            
                 
         </Navbar>
     )
